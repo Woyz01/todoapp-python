374692db631ccb3bd298f503ea6cbc1d023eb4e3
Woyz01 <01cihan23@gmail.com>
2025-11-16 21:47:33 +0300

    Delete sil.py

--- sil.py
+++
@@ -1,8 +0,0 @@
-while True:
-
-    try:
-        myname = str(input("Pls enter your name: "))
-        myage = str(input("Pls enter your age: "))
-        print(myname, myage)
-    except:
-        print("Pls enter wrong input")
